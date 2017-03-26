94ec180d6062dd47c6072753b60bceaab5332e6e
jenilee <jenilee.hsu@gmail.com>
2017-03-26 13:51:55 -0700

    added comments controller and model

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -3,5 +3,7 @@ class Milestone < ApplicationRecord
   belongs_to :kid
 
   has_many :images
+
+  has_many :comments
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170324020944) do
+ActiveRecord::Schema.define(version: 20170326203856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "name"
+    t.text     "text"
+    t.integer  "milestone_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "images", force: :cascade do |t|
     t.integer  "milestone_id"
