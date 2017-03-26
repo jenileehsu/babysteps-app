62bf984e42a2872fce33371b5219d9e47cde7cba
jenilee <jenilee.hsu@gmail.com>
2017-03-26 14:29:49 -0700

    created comment fields and create action on milestone show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+
+  def create
+    kid = Kid.find_by(id: params[:kid_id])
+    milestone = Milestone.find_by(id: params[:milestone_id])
+    comment = Comment.create(
+      name: params[:name],
+      text: params[:text],
+      milestone_id: params[:milestone_id]
+      )
+    redirect_to "/kids/#{kid.id}/milestones/#{milestone.id}"
+  end
+
 end
