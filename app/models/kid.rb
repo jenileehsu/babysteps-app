1099d618b1f2adf8ba406e83b65f37278f311097
jenilee <jenilee.hsu@gmail.com>
2017-03-27 21:15:59 -0700

    edited default avatar for kids

--- app/models/kid.rb
+++ app/models/kid.rb
@@ -6,7 +6,7 @@ class Kid < ApplicationRecord
 
   has_attached_file :avatar,
                     :styles => { :medium => "150x150>", :thumb => "44x44#" },
-                    :default_url => "/images/:style/missing.png"
+                    :default_url => "/assets/baby_avatar.png"
 
   validates_attachment :avatar, :presence => true,
                        :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] },
