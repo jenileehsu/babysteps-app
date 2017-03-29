e95d277208f438610b4b3996132203703ca4b19c
jenilee <jenilee.hsu@gmail.com>
2017-03-28 21:31:49 -0700

    edited photo sizes for milestone images on kid timeline

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,7 +3,7 @@ class Image < ApplicationRecord
   belongs_to :milestone
 
   has_attached_file :avatar,
-                    :styles => { :medium => "150x150>", :thumb => "75x75#" },
+                    :styles => { :medium => "150x150>", :thumb => "75x75#", :icon => "100x100#"},
                     :default_url => "/images/:style/missing.png"
 
   validates_attachment :avatar, :presence => true,
